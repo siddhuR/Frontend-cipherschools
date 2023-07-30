4111777a65e4a0851087ae0964f3a983e6b2e9b2
Routhu Siddhartha <routhusiddhartha@gmail.com>
2023-07-30 14:02:29 +0530

    scoping

---
+++ JavaScript/scoping.js
@@ -0,0 +1,67 @@
+// // Global scope and functional scope
+
+// var age = 10;
+
+// if(true) {
+//     var email='sid@gmail.com'
+//     console.log(age);
+// }
+
+// function display(){
+//     var name = 'Harry';
+//     console.log(name, age);
+// }
+
+// console.log(name, email);
+// console.log(typeof name);
+// display();
+// -----------------------------------------------
+// var age = 10;
+
+// var name= 'Sid';
+// function display(){
+//     var name = 'routhu';
+//     console.log(name, age);
+// }
+
+// console.log(name);
+// display();
+
+//---------------------------------------------
+
+// ES6 features
+// let, const, arroww function, destructuring of objects, rest syntax
+
+// let age = 10;
+// if(true) {
+//     age = 15;
+//     console.log(age);
+// }
+// function display() {
+//     let age = 12;
+//     console.log(age);
+//     if(true) {
+//         let age = 20;
+//         var rollNo = 'AB1234';
+//         console.log(age);
+//     }
+//     console.log(rollNo);
+// }
+// console.log(rollNo);
+
+// //console.log(age);
+// display();
+// console.log(age);
+
+// ----------------------------------------------------------
+
+// const
+
+if(true){
+    
+    const PI = 3.14;
+    const GV = 9.8;
+    console.log(PI);
+
+}
+console.log(PI)
