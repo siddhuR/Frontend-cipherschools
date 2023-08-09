d8f101ed1ce801f4e88c0fe68825520098027452
Routhu Siddhartha <routhusiddhartha@gmail.com>
2023-08-09 23:56:36 +0530

    functions1

---
+++ JavaScript/function.js
@@ -0,0 +1,20 @@
+//functions
+'use strict';
+
+// function declaration
+function greeting() {
+    console.log("Good Morning");
+}
+greeting();
+// greet();
+
+// // function expression
+var greet = function() {
+    console.log('Greetings Everyone!');
+}
+
+console.log(str);
+//console.log(val);
+
+var str =  'some value';
+//let val = 'let value';
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+function greeting() {
+//     console.log("Good Morning");
+// }
+// greeting();
+// greet();
+// // function expression
+// var greet = function() {
+//     console.log('Greetings Everyone!');
+// }
